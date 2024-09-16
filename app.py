6a7c529764bd3ece6810c62d957036f800e57b89
V.T.isa <135535849+vtisa@users.noreply.github.com>
2024-09-16 17:00:15 -0500

    update

--- app.py
+++ app.py
@@ -65,5 +65,5 @@ def eliminar_registro(dni):
     return redirect(url_for('administrar'))
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
+    port = int(os.environ.get('PORT', 5000))    
+    app.run(host='0.0.0.0', port=port, debug=True)
